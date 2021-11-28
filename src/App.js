08d8195a9c83187ba9013c11972094b7402fecca
Johnson Pasanna <johnsonpasanna1997@gmail.com>
2021-11-28 22:58:50 +0530

    connected API

--- src/App.js
+++ src/App.js
@@ -6,27 +6,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "1",
-        },
-        {
-          name: "Zombie",
-          id: "2",
-        },
-        {
-          name: "Chibula",
-          id: "3",
-        },
-      ],
+      //first we hardcoded the mosters name but in real life we will get the data from the backened (server , database etc) we will use a resource where it will give 10 users data without actually building any kind of backended server
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((responce) => responce.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((mov) => (
-          <h1 key={mov.id}>{mov.name}</h1>
+          <h1 key={mov.id}>{mov.address.geo.lat}</h1>
         ))}
       </div>
     );
